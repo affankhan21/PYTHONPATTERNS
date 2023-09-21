e09258e5696c9a3eab564e65ab7293192f4404a7
AFFAN SIRAJUDDIN KHAN <132735435+affankhan21@users.noreply.github.com>
2023-09-21 14:48:34 +0530

    Add files via upload

---
+++ starpattern7.py
@@ -0,0 +1,30 @@
+"""
+OUTPUT:
+  *   *   *   *   *   
+    *   *   *   *   
+      *   *   *   
+        *   *   
+          *   
+        *   *   
+      *   *   *   
+    *   *   *   * 
+    """
+
+
+
+
+
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(" ",end=" ")
+    for j in range(1,7-i):
+        print("*  ",end=" ")
+    print()    
+    
+for i in range(1,5):
+    for j in range(1,7-i):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*  ",end=" ")
+    print()   
